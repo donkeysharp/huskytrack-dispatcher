76c12607c46346c25b2cb447e21e11b75ce2302d
Sergio Guillen <serguimant@gmail.com>
2016-09-21 23:57:22 -0400

    Refactored mqtt operation on private methods

--- MqttListener.js
+++ MqttListener.js
@@ -5,6 +5,7 @@ var Observable = require('./Observable');
 
 var truckRegex = /truck\/(.+)\/(speed|position|fuel)/;
 
+
 class MqttListener  extends Observable {
   constructor(config, topics) {
     super();
@@ -14,10 +15,7 @@ class MqttListener  extends Observable {
     this.connected = false;
   }
 
-  start() {
-    if (this.connected) {
-      return;
-    }
+  __initializeMqtt() {
     var mqttUrl = `mqtt://${this.config.host}:${this.config.port}`;
     this.client = mqtt.connect(mqttUrl, {
       username: this.config.username,
@@ -25,6 +23,7 @@ class MqttListener  extends Observable {
     });
 
     var self = this;
+    // Init connection events
     this.client.on('connect', function() {
       console.log('INFO: Connected to mqtt broker.');
       self.connected = true;
@@ -32,6 +31,20 @@ class MqttListener  extends Observable {
       console.log(`INFO: Subscribing to: ${JSON.stringify(self.topics)}`);
       self.client.subscribe(self.topics);
     });
+
+    // unsubscribe if client goes offline
+    this.client.on('offline', function() {
+      console.log('INFO: Connection with mqtt broker has been lost.');
+
+      self.client.unsubscribe(self.topics);
+      self.connected = false;
+    });
+
+    this.__mqttListen()
+  }
+
+  __mqttListen() {
+    var self = this;
 
     this.client.on('message', function(topic, message) {
       if (!truckRegex.test(topic)) { return; }
@@ -45,12 +58,13 @@ class MqttListener  extends Observable {
       console.log(`Message from ${topic}, notifying...`);
       self.notifyObservers(topic, message, info);
     });
+  }
 
-    this.client.on('offline', function() {
-      console.log('INFO: Connection with mqtt broker has been lost.');
-      self.client.unsubscribe(self.topics);
-      self.connected = false;
-    });
+  start() {
+    if (this.connected) {
+      return;
+    }
+    this.__initializeMqtt();
   }
 }
 
